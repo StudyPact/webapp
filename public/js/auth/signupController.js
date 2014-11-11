547f1fcc6ec9b0ae1f69a7707b8ca5906ee67eb6
Tobias Hoenisch <tobias@hoenisch.at>
2014-11-11 14:47:58 +0900

    duplicate email address and server error warning

--- public/js/auth/signupController.js
+++ public/js/auth/signupController.js
@@ -3,22 +3,24 @@ var module = angular.module("studypact");
 module.controller("SignupController", ["$scope", "$state","UserService","AuthenticationService", function ($scope, $state, userService, authenticationService) {
 
     $scope.signup = function () {
-
         userService.createUser($scope.user)
             .then(function(response){
                 console.log("Registered new user:", response);
                 authenticationService.login($scope.user)
                 .then(function(accessToken){
                     $state.go("loggedIn");
-                },
-                function(error) {
-                    //TODO dynamically set error text
-                    console.log(error);
-                    $scope.loginError = true;
                 });
             },
             function(error) {
-                console.log(error);
+                console.log("signup error:",error.status);
+                $scope.error = {};
+                if (error.status===409){
+                    console.log("409")
+                    $scope.error.mailInUseError=true;
+                }
+                else{
+                    $scope.error.serverError=true;
+                };
             });
     };
 }]);
