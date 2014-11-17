566380fb6b90a94f3b4ad10ee94022b237caeb4b
Tobias Hoenisch <tobias@hoenisch.at>
2014-11-17 12:30:58 +0900

    removing debug

--- public/app/components/cardDisplay/cardController.js
+++ public/app/components/cardDisplay/cardController.js
@@ -1,6 +1,5 @@
 var module = angular.module("studypact");
 
 module.controller("CardController", function($scope, $state) {
-  console.log($state);
   $scope.activeCards = $state.current.data.activeBoxes;
 });
