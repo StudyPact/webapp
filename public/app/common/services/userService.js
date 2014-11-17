b1cf0721fcd525adb0cc7ff1ee895828ca6b60d8
Tobias Hoenisch <tobias@hoenisch.at>
2014-11-17 11:59:29 +0900

    add load user and add to profileController

--- public/app/common/services/userService.js
+++ public/app/common/services/userService.js
@@ -1,56 +1,62 @@
-angular.module('studypact').factory('UserService', ["$resource","$log","CacheService", 
+angular.module('studypact').factory('UserService', ["$resource", "$log", "CacheService",
   function($resource, $log, CacheService) {
-  var host = clientConfig.host;
+    var host = clientConfig.host;
 
-  var error_handler = function(err) {
-    console.error(err);
-  };
+    var error_handler = function(err) {
+      console.error(err);
+    };
 
-  var functions = {
-    createUser: function(user) {
-      var User = $resource(clientConfig.host + '/api/users');
-      return User.save(user).$promise;
-    },
+    var functions = {
+      createUser: function(user) {
+        var User = $resource(clientConfig.host + '/api/users');
+        return User.save(user).$promise;
+      },
 
-    loadUsers: function() {
-      $log.debug("Loading Users");
-      var FilteredUsers = $resource(host + '/api/users?fields=_id,displayname,picture');
-      //return CacheService.getAndApply("users", FilteredUsers.query());
-      return FilteredUsers.query();
-    },
+      loadUser: function(id) {
+        var User = $resource(host + '/api/users/' + id);
+        return User.get();
+      },
 
-    loadFriendRequests: function() {
-      $log.debug("Loading Friend Requests");
-      var FriendRequests = $resource(host + '/api/friends?friend_status=incoming_request');
-      //return CacheService.getAndApply("friendRequests", FriendRequests.query());
-      return FriendRequests.query();
-    },
+      loadUsers: function() {
+        $log.debug("Loading Users");
+        var FilteredUsers = $resource(host + '/api/users?fields=_id,displayname,picture');
+        //return CacheService.getAndApply("users", FilteredUsers.query());
+        return FilteredUsers.query();
+      },
 
-    loadFriends: function() {
-      $log.debug("Loading Friends");
-      var Friends = $resource(host + '/api/friends?friend_status=confirmed');
-      //return CacheService.getAndApply("friends", Friends.query());
-      return Friends.query();
-    },
+      loadFriendRequests: function() {
+        $log.debug("Loading Friend Requests");
+        var FriendRequests = $resource(host + '/api/friends?friend_status=incoming_request');
+        //return CacheService.getAndApply("friendRequests", FriendRequests.query());
+        return FriendRequests.query();
+      },
 
-    sendFriendRequest: function (id) {
-      $log.debug("Sending Friend Request");
-      var Friend = $resource(host + '/api/friends/'+id);
-      var savePromise = Friend.save({});
-      savePromise.$promise.then(functions.loadUsers, error_handler);
-      return savePromise.$promise;
-    },
+      loadFriends: function() {
+        $log.debug("Loading Friends");
+        var Friends = $resource(host + '/api/friends?friend_status=confirmed');
+        //return CacheService.getAndApply("friends", Friends.query());
+        return Friends.query();
+      },
 
-    acceptFriendRequest: function (id) {
-      $log.debug("Accepting Friend Request");
-      var FriendAccept = $resource(host + '/api/friends/'+id+"/accept");
-      var acceptPromise = FriendAccept.get({});
-      acceptPromise.$promise.then(functions.loadUsers, error_handler);
-      acceptPromise.$promise.then(functions.loadFriends, error_handler);
-      acceptPromise.$promise.then(functions.loadFriendRequests, error_handler);
-      return acceptPromise.$promise;
-    },
+      sendFriendRequest: function(id) {
+        $log.debug("Sending Friend Request");
+        var Friend = $resource(host + '/api/friends/' + id);
+        var savePromise = Friend.save({});
+        savePromise.$promise.then(functions.loadUsers, error_handler);
+        return savePromise.$promise;
+      },
 
-  };
-  return functions;
-}]);
+      acceptFriendRequest: function(id) {
+        $log.debug("Accepting Friend Request");
+        var FriendAccept = $resource(host + '/api/friends/' + id + "/accept");
+        var acceptPromise = FriendAccept.get({});
+        acceptPromise.$promise.then(functions.loadUsers, error_handler);
+        acceptPromise.$promise.then(functions.loadFriends, error_handler);
+        acceptPromise.$promise.then(functions.loadFriendRequests, error_handler);
+        return acceptPromise.$promise;
+      },
+
+    };
+    return functions;
+  }
+]);
