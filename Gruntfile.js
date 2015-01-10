1c14f05f727a4ea54ecfb533d89cf652b70b2d6d
Michael Sperk <michael.sperk@visalyze.org>
2015-01-10 14:34:06 +0100

    generate config from grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks("grunt-contrib-less");
     grunt.loadNpmTasks("grunt-contrib-watch");
     grunt.loadNpmTasks('grunt-include-source');
+    grunt.loadNpmTasks('grunt-ng-constant');
 
     grunt.initConfig({
 
@@ -49,10 +50,32 @@ module.exports = function (grunt) {
                     'public/index.html': 'public/assets/index.tpl.html'
                 }
             }
+        },
+        ngconstant: {
+            // Options for all targets
+            options: {
+                wrap: false,
+                deps: false,
+                name: 'studypact'
+            },
+            // Environment targets
+            config: {
+                options: {
+                    dest: 'public/app/config.js'
+                },
+                constants: {
+                    Config: {
+                        host: "https://studypact-rest-test.herokuapp.com",
+                        api_version: 2,
+                        client_id: "ZxaoIsUoshMow5Dx",
+                        client_secret: "njNA842Msr6IVbyfO3X3Ag32Q66jl6IO"
+                    }
+                }
+            }
         }
     });
     grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', [ 'less:studypact', 'includeSource:dev']);
-    grunt.registerTask('watch' ['watch'])
+    grunt.registerTask('build', [ 'less:studypact', 'ngconstant:config', 'includeSource:dev']);
+    grunt.registerTask('watch' ['watch']);
     grunt.registerTask('heroku:development', ['build']);
 };
--- public/app/config.js
+++ public/app/config.js
@@ -1,7 +1,5 @@
-var module = angular.module("studypact");
-module.value('Config', {
-  host: "https://studypact-rest-test.herokuapp.com",
-  api_version: 2,
-  client_id: "ZxaoIsUoshMow5Dx",
-  client_secret: "njNA842Msr6IVbyfO3X3Ag32Q66jl6IO"
-});
+angular.module('studypact')
+
+.constant('Config', {host:'https://studypact-rest-test.herokuapp.com',api_version:2,client_id:'ZxaoIsUoshMow5Dx',client_secret:'njNA842Msr6IVbyfO3X3Ag32Q66jl6IO'})
+
+;
