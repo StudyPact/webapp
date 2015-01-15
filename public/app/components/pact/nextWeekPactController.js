331469c67d3a86147ba5f08df2a83d8e7bdebdfe
Toby Hoenisch <tobias@hoenisch.at>
2015-01-15 11:25:26 +0100

    Just some cleanup

--- public/app/components/pact/nextWeekPactController.js
+++ public/app/components/pact/nextWeekPactController.js
@@ -1,11 +1,11 @@
 var module = angular.module("studypact");
-HOUR = 60;
-MAX_HOURS = 10;
-MIN_HOURS = 1;
+var HOUR = 60;
+var MAX_HOURS = 10;
+var MIN_HOURS = 1;
 
-STAKES_INCREMENT = 5;
-MAX_STAKES = 50;
-MIN_STAKES = 5;
+var STAKES_INCREMENT = 5;
+var MAX_STAKES = 50;
+var MIN_STAKES = 5;
 
 module.controller("NextWeekPactController", ["$scope", "$resource","UserService", 
   function($scope, $resource, UserService) {
@@ -13,7 +13,6 @@ module.controller("NextWeekPactController", ["$scope", "$resource","UserService"
   $scope.loadUser = function(id) {
     $scope.user = UserService.loadUser(id);
     $scope.changed = false;
-
   };
 
   $scope.save = function() {
@@ -23,7 +22,9 @@ module.controller("NextWeekPactController", ["$scope", "$resource","UserService"
       pact_duration: $scope.user.pact_duration
     };
     $scope.user=UserService.saveUser(userUpdate);
-    $scope.changed = false;
+    $scope.user.$promise.then(function(){
+      $scope.changed = false;
+    });
   };
 
   $scope.loadUser("me");
@@ -31,29 +32,33 @@ module.controller("NextWeekPactController", ["$scope", "$resource","UserService"
   $scope.increaseHours = function() {
     $scope.changed = true;
     $scope.user.pact_duration += HOUR;
-    if ($scope.user.pact_duration>HOUR * MAX_HOURS)
+    if ($scope.user.pact_duration>HOUR * MAX_HOURS){
       $scope.user.pact_duration = HOUR * MAX_HOURS;
-  }
+    }
+  };
 
   $scope.decreaseHours = function() {
     $scope.changed = true;
     $scope.user.pact_duration -= HOUR;
-    if ($scope.user.pact_duration < HOUR * MIN_HOURS)
+    if ($scope.user.pact_duration < HOUR * MIN_HOURS){
       $scope.user.pact_duration = HOUR * MIN_HOURS;
-  }
+    }
+  };
 
   $scope.increaseStakes = function() {
     $scope.changed = true;
     $scope.user.pact_stakes+= STAKES_INCREMENT;
-    if ($scope.user.pact_stakes> MAX_STAKES)
+    if ($scope.user.pact_stakes> MAX_STAKES){
       $scope.user.pact_stakes = MAX_STAKES;
-  }
+    }
+  };
 
   $scope.decreaseStakes = function() {
     $scope.changed = true;
     $scope.user.pact_stakes-= STAKES_INCREMENT;
-    if ($scope.user.pact_stakes<MIN_STAKES)
+    if ($scope.user.pact_stakes<MIN_STAKES){
       $scope.user.pact_stakes = MIN_STAKES;
-  }
+    }
+  };
 
 }]);
